a2f25e1445a665146f38f36a49f2b802eb5b2896
imrisaac@gmaik.com <ireed@altavian.com>
2019-09-03 22:57:56 -0400

    messed it up

--- src/main.c
+++ src/main.c
@@ -2,8 +2,11 @@
 #include <gitversion.h>
 #include <stdio.h>
 
+
 int main() {
-  printf("This app is built from git revision: %s at %s\n", GIT_REVISION,
+  
+  printf("This app is built from git commit: %s, commit date %s\n", GIT_REVISION,
          BUILD_TIMESTAMP);
   return 0;
+
 }

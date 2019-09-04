dfcbac77ac906291391710789bb4604ed98008c2
imrisaac@gmaik.com <ireed@altavian.com>
2019-09-04 09:52:50 -0400

    cleanup

--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@
 int main() {
 
   printf("Commit: %s, %s\n", GIT_REVISION, COMMIT_TIMESTAMP);
-  printf("Version: %s %s %s\n", THISBUILD, COMPILE_DATE, COMPILE_TIME);
+  printf("Version: %s
+   %s %s\n", THISBUILD, COMPILE_DATE, COMPILE_TIME);
   return 0;
 
 }
